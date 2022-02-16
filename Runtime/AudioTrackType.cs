571bf01d3fe8aec0e7bfd225daea348eb766bdaa
Paku <64353979+PakuBK@users.noreply.github.com>
2022-02-16 16:00:10 +0100

    Add Package from Unity

---
+++ Runtime/AudioTrackType.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Audio
+{
+    public enum AudioTrackType
+    {
+        None,
+        SoundTrack,
+        Player,
+        Enviroment,
+        Entitys,
+        SFX
+    }
+}
---
+++ Runtime/AudioType.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio
+{
+    public enum AudioType
+    {
+        None,
+        ST_01,
+        SFX_Jump,
+        SFX_Attack,
+        SFX_Death
+        // ... custom audio types here
+    }
+}
